72bcf6ef3406e4d3c21d570287e6c8bf9f2d01f3
vladrepin26 <vladyslav.riepin@gmail.com>
2020-09-14 12:11:44 +0300

    Change a Model

--- src/products/models.py
+++ src/products/models.py
@@ -6,4 +6,5 @@ class Product(models.Model):
     title = models.CharField(max_length=120)  # max_length = required
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10000, decimal_places=2)
-    summary = models.TextField()
+    summary = models.TextField(blank=False, null=False)
+    featured = models.BooleanField()  # null=True, default=True
