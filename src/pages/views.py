3f620f05cb174943678fa833f507ac8e2f2cf7f9
vladrepin26 <vladyslav.riepin@gmail.com>
2020-09-14 13:10:29 +0300

    URL Routing and Requests

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,5 +3,19 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
+    print(*args, **kwargs)
+    print(request.user)
     return HttpResponse('<h1>Hello world</h1>')
+
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse('<h1>Contact page</h1>')
+
+
+def about_view(request, *args, **kwargs):
+    return HttpResponse('<h1>About page</h1>')
+
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse('<h1>Social page</h1>')
