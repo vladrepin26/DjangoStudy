acf488e11099401923d6bc202317f87469c451f4
vladrepin26 <vladyslav.riepin@gmail.com>
2020-09-14 16:01:58 +0300

    Using Conditions in a Template

--- src/pages/views.py
+++ src/pages/views.py
@@ -16,7 +16,10 @@ def contact_view(request, *args, **kwargs):
 
 def about_view(request, *args, **kwargs):
     my_context = {
-        "title": "About page",
+        "my_text": "This is about us",
+        "this_is_true": True,
+        "my_number": 123,
+        "my_list": [1313, 4231, 312, "Abc"],
     }
     return render(request, "about.html", my_context)
 
